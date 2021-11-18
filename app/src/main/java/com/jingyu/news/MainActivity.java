74851276508063abe610a6586b432b1f16dae2f4
eigen <jingyu.pang@tufts.edu>
2021-11-17 19:13:53 -0500

    building retrofit client

---
+++ app/src/main/java/com/jingyu/news/MainActivity.java
@@ -0,0 +1,49 @@
+package com.jingyu.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.jingyu.news.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        binding.submitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String name = binding.nameEditText.getText().toString();
+//                String email = binding.nameEditText.getText().toString();
+//                binding.welcomeTextView.setText("Welcome " + name + ", your email is: " + email);
+//            }
+//        });
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        return navController.navigateUp();
+    }
+}
---
+++ app/src/main/java/com/jingyu/news/network/RetrofitClient.java
@@ -0,0 +1,9 @@
+package com.jingyu.news.network;
+
+public class RetrofitClient {
+
+    private static final String API_KEY = "183463fdc9694c38812a626c14d753aa";
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+
+
+}
